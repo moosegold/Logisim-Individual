226d5eaa1fa83a81e61a5754d0a53b662db00c88
Mitchell LunBeck <moosegit@outlook.com>
2019-10-04 16:13:30 -0700

    Changed SidebarButtons to draw to their own canvas. ComponentSidebar now draws the button bitmap to its own canvas. There is a rendering order issue currently with the debug text.

--- app/src/main/java/LogiSim/SidebarButton.java
+++ app/src/main/java/LogiSim/SidebarButton.java
@@ -2,56 +2,66 @@ package LogiSim;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 
 public class SidebarButton {
 
-    final AbstractScreenPartition partition;
+    final String action;
+
+    private final AbstractScreenPartition partition;
+    Bitmap image;
+    private Canvas canvas;
 
     final ScreenPoint point;
     final int length;
-    final String action;
-    final int Rresource;
+    private final int Rresource;
 
-    final DebugTextDrawer debugText;
+    private final DebugTextDrawer debugText;
 
     public SidebarButton(ScreenPoint point, int length, String action, int Rresource, AbstractScreenPartition partition) {
         this.point = point;
         this.length = length;
         this.action = action;
         this.Rresource = Rresource;
-
+        createCanvas();
         this.partition = partition;
         this.debugText = new DebugTextDrawer(point, partition.canvas, true);
     }
 
+    private void createCanvas() {
+        image = Bitmap.createBitmap(length, length, Bitmap.Config.ARGB_8888);
+        canvas = new Canvas(image);
+    }
+
     public void draw() {
+        createCanvas();
         debugText.addText("Button Loc: " + point);
         debugText.addText("Button Size: " + length);
-        drawImage();
+        drawComponentImage();
         drawBounds();
 
-        debugText.draw(partition.canvas);
+        debugText.draw(canvas);
     }
 
-    void drawImage() {
-        Rect orgImgRect = new Rect(point.x, point.y, getImage().getWidth(), getImage().getWidth());
-        Rect transformImgRect = new Rect(point.x, point.y, point.x + length, point.y + length);
-        transformImgRect.offsetTo(point.x, point.y + (length / 4));
+    private void drawComponentImage() {
+        Rect orgImgRect = new Rect(0, 0, getImage().getWidth(), getImage().getWidth());
+        Rect transformImgRect = new Rect(0, 0, length, length);
+        transformImgRect.offsetTo(0, length / 4);
         debugText.addText("Img size: " + new Size(transformImgRect.width(), transformImgRect.height()));
         debugText.addText("Img at: " + new ScreenPoint(transformImgRect.left, transformImgRect.top));
-        partition.canvas.drawBitmap(getImage(), orgImgRect, transformImgRect, null);
+        canvas.drawBitmap(getImage(), orgImgRect, transformImgRect, null);
     }
 
-    void drawBounds() {
-        Rect bounds = Util.getRect(point, new Size(length, length));
+    private void drawBounds() {
+        Rect bounds = new Rect(0, 0, length - 1, length - 1);
         Paint borderPaint = new Paint();
         borderPaint.setColor(Color.BLUE);
         borderPaint.setStyle(Paint.Style.STROKE);
 
-        partition.canvas.drawRect(bounds, borderPaint);
+        canvas.drawRect(bounds, borderPaint);
     }
 
     private Bitmap getImage() {
