6bf6a39ed1ab106f6ca1b68e469437f76719ebb3
Mitchell LunBeck <moosegit@outlook.com>
2019-10-13 16:21:08 -0700

    Highligting wire dest

--- app/src/main/java/logisim/state/modes/WireMode.java
+++ app/src/main/java/logisim/state/modes/WireMode.java
@@ -1,11 +1,14 @@
 package logisim.state.modes;
 
+import android.graphics.Paint;
+
 import logisim.Grid;
 import logisim.state.StateManager;
 import logisim.tiles.Tile;
 import logisim.tiles.components.Component;
 import logisim.util.Paints;
 import logisim.util.ScreenPoint;
+import logisim.util.Util;
 
 public class WireMode extends AbstractMode {
 
@@ -48,6 +51,11 @@ public class WireMode extends AbstractMode {
 
     @Override
     public void draw() {
+        drawLine();
+        drawSelection();
+    }
+
+    private void drawLine() {
         stateManager.debugText.addText("Source: " + stateManager.getTouchedObjectStart());
         Component source = (Component) stateManager.getTouchedObjectStart();
         ScreenPoint start = grid.convertToScreenPoint(source.getOutputPos());
@@ -55,6 +63,18 @@ public class WireMode extends AbstractMode {
         stateManager.debugText.addText("Start: " + start);
         stateManager.debugText.addText("End: " + end);
         stateManager.canvas.drawLine(start.x, start.y, end.x, end.y, Paints.WIRE);
+    }
+
+    private void drawSelection() {
+        Util.drawTileOutline((Tile) stateManager.getTouchedObjectStart(), grid, stateManager.canvas, Paints.TILE_OUTLINE_SOURCE);
+        ScreenPoint screenPoint = stateManager.getDragPoint();
+        if (grid.containsTouch(screenPoint)) {
+            Tile hoverTile = grid.getTile(grid.convertToGridPoint(screenPoint));
+            if (hoverTile instanceof Component) {
+                Paint paint = isLoopSafe((Tile) stateManager.getTouchedObjectStart(), hoverTile) ? Paints.TILE_OUTLINE_ALLOW_PLACE : Paints.TILE_OUTLINE_DENY_PLACE;
+                Util.drawTileOutline(grid.getTile(grid.convertToGridPoint(screenPoint)), grid, stateManager.canvas, paint);
+            }
+        }
     }
 
     @Override
