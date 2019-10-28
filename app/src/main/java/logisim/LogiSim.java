830f84c90c1900b6bf42befc0046eb029bf8c6b1
Mitchell LunBeck <moosegit@outlook.com>
2019-10-27 18:09:38 -0700

    Turned off debug text

--- app/src/main/java/logisim/LogiSim.java
+++ app/src/main/java/logisim/LogiSim.java
@@ -18,7 +18,7 @@ public class LogiSim extends Activity {
 
     private static final int GRID_WIDTH_TILES = 12;
 
-    public static final boolean DEBUG_TEXT_ENABLED = true;
+    public static final boolean DEBUG_TEXT_ENABLED = false;
 
     private ScreenManager screenManager;
     private StateManager stateManager;
